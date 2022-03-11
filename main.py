744c29a75a43864f3c5047ae287ae4877d916769
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-11 13:11:48 +0100

    Cambios en el main

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-
-variable= int(input("Por favor, seleccione el ejercicio que desea ejecutar (1-3):"))
-if variable==1:
-  import Ejercicio1
-if variable ==2:
-  import Ejercicio2
-if variable ==3:
-  import Ejercicio3
-else:
-  print("Por favor, eliga uno de los tres ejercicios disponibles.")
-
 from Clases.Ejercicio1 import *
 from Clases.Ejercicio2 import *
 from Clases.Ejercicio3 import *
 
-if __name__ == 
+if __name__ == "main"
+  print("Selecciona el ejercicio que desea ejecutar entre el 1 y el 3:")
+  if __name__ ==1:
+    import Ejercicio1()
+  if __name__==2:
+    import Ejercicio2()
+  if __name__==3:
+    import Ejercicio3()
